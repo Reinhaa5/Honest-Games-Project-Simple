da1d3ee6aa19e4e0f0cf522794cc6228dec34f64
Reinhaa5 <94755067+Reinhaa5@users.noreply.github.com>
2026-05-03 22:32:39 -0400

    Added Frameworks
    Added Astro and tailwind and started refactoring. Will merge once caught up.

--- Honest-Games-Site/scripts/core.js
+++
@@ -1,8 +0,0 @@
-function mobileNavDisplay() {
-    let x = document.getElementById("main-nav");
-    if (x.style.display === "flex") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "flex";
-    }
-}
